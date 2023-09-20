2f9a4b5595055af55048205c536ddae1f6a06a14
David Garcia <davidgarciab@gmail.com>
2023-09-20 16:26:09 +0200

    feat: support latest Nx version 16.8.1

--- src/tests/nx-gcp-cache.test.ts
+++ src/tests/nx-gcp-cache.test.ts
@@ -1,52 +1,23 @@
-import { NxJsonConfiguration, getPackageManagerCommand } from '@nx/devkit';
+import { NxJsonConfiguration } from '@nx/devkit';
 import {
-  cleanup,
-  patchPackageJsonForPlugin,
+  ensureNxProject,
   readJson,
-  runCommandAsync,
-  tmpProjPath,
+  runNxCommandAsync,
 } from '@nx/plugin/testing';
-import { execSync } from 'child_process';
-import { ensureDirSync } from 'fs-extra';
-import { dirname } from 'node:path';
-
-function runNxNewCommand() {
-  const localTmpDir = dirname(tmpProjPath());
-
-  return execSync(
-    `npx nx new proj --nx-workspace-root=${localTmpDir} --no-interactive --skip-install --collection=@nx/workspace --npmScope=proj --preset=empty`,
-    {
-      cwd: localTmpDir,
-    },
-  );
-}
-
-function runPackageManagerInstall(silent: boolean = true) {
-  const pmc = getPackageManagerCommand('npm');
-  const install = execSync(pmc.install, {
-    cwd: tmpProjPath(),
-    ...(silent ? { stdio: ['ignore', 'ignore', 'ignore'] } : {}),
-  });
-
-  return install ? install.toString() : '';
-}
 
 describe('nx-gcp-cache e2e', () => {
   beforeAll(() => {
-    ensureDirSync(tmpProjPath());
-    cleanup();
-    runNxNewCommand();
-    patchPackageJsonForPlugin('nx-gcp-cache', '');
-    runPackageManagerInstall();
+    ensureNxProject('nx-gcp-cache', '');
+
   });
 
   afterAll(async () => {
-    await runCommandAsync('npx nx reset');
+    runNxCommandAsync('reset');
   });
 
   it('should init nx-gcp-cache', async () => {
     const bucketName = 'bucket-name/cache-folder';
-    await runCommandAsync(`npx nx generate nx-gcp-cache:init --gcpBucket=${bucketName}`);
+    await runNxCommandAsync(`generate nx-gcp-cache:init --gcpBucket=${bucketName}`);
 
     const nxJson: NxJsonConfiguration = readJson('nx.json');
     expect(nxJson.tasksRunnerOptions!.default.runner).toEqual('nx-gcp-cache');
